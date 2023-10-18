fc5e38e60cf9c252e1709740b8794fb7f19791b2
Dilshad Matkulov <dilshadmatkulov@.gmail.com>
2023-10-18 22:17:31 +0600

    webinar-30

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import Person from "./Person/Person";
 import './App.css';
 import {useState} from "react";
 
+interface Person {
+    name: string;
+    age: number;
+    hobby: string;
+}
+
 function App() {
-    const [people, setPeople] = useState([
+    const [people, setPeople] = useState<Person[]>([
         {name: 'Jane', age: 28, hobby: 'Knitting'},
         {name: 'John', age: 32, hobby: 'Video games'},
     ]);
+
+    const [numbers, setNumbers] = useState<number[]>([])
     const changeName = () => {
             setPeople([
             {name: 'Jane Doe', age: 28, hobby: 'Knitting'},
@@ -14,12 +22,22 @@ function App() {
         ]);
     };
 
+    const addNumber = () => {
+        setNumbers((prevState) => {
+            return [...prevState, 20];
+        })
+    };
+
     return (
         <div className="App">
             <Person name={people[0].name} age={people[0].age}><strong>Hobby: </strong>{people[0].hobby}</Person>
             <Person name={people[1].name} age={people[1].age}><strong>Hobby: </strong>{people[1].hobby}</Person>
             <div>
+                {numbers.join(', ')}
+            </div>
+            <div>
                 <button onClick={changeName}>Change name</button>
+                <button onClick={addNumber}>Change number</button>
             </div>
         </div>
     );
