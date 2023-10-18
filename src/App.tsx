9e11a8dc65f5da9a9009f56f6757d33220500ecc
Dilshad Matkulov <dilshadmatkulov@.gmail.com>
2023-10-18 22:02:47 +0600

    Add Person

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Person from "./Person/Person";
+import './App.css';
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [people, setPeople] = useState([
+        {name: 'Jane', age: 28, hobby: 'Knitting'},
+        {name: 'John', age: 32, hobby: 'Video games'},
+    ]);
+    const changeName = () => {
+            setPeople([
+            {name: 'Jane Doe', age: 28, hobby: 'Knitting'},
+            {name: 'John Doe', age: 32, hobby: 'Video games'},
+        ]);
+    };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="App">
+            <Person name={people[0].name} age={people[0].age}><strong>Hobby: </strong>{people[0].hobby}</Person>
+            <Person name={people[1].name} age={people[1].age}><strong>Hobby: </strong>{people[1].hobby}</Person>
+            <div>
+                <button onClick={changeName}>Change name</button>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
